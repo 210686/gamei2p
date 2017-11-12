17dd7fe5d015c01fae740a2b04d9cf807f3a2651
210686 <210686@hkis.edu.hk>
2017-11-13 06:32:22 +0800

    #

--- Summative 2c.py
+++ Summative 2c.py
@@ -1,21 +1,27 @@
+#the necessary variables for the code
 counter=0
 correct = 0
 i = 0
+
+#the answers
 answer = ["true","false","false","true","true","false","false","false","false","true","true","true","true","false","true","false"]
+
+#the body of the code
 while counter<15:
-    f=open("testfile.txt","r")
+    f=open("testfile.txt","r")#open the file
     lines=f.readlines()
-    print (lines[i*4+3]+lines[i*4]+lines[i*4+1]+lines[i*4+2]+lines[i*4+3])
-    f.close()
-    ans = input("").lower()
+    print (lines[i*4+3]+lines[i*4]+lines[i*4+1]+lines[i*4+2]+lines[i*4+3])#printing out the lines on the text file
+    f.close()#close the file
+    ans = input("").lower()#inputting user's answer
     while ans != "true" and ans != "false":
         print("Invalid")
         ans = input("").lower()
-    if ans == answer[i]:
+    if ans == answer[i]:#correct
         print("Correct!")
         correct += 1
-    else:
+    else:#wrong
         print("Incorrect")
     i += 1
     counter +=1
 print("Your got",correct,"correct, which is",str(round(correct/15,2)*100)+"%.")
+#point showed in npercentage form
